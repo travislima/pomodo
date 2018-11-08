aaa76fe6e3cc2aa763e1854a37ddbccc165ce00c
Travis Lima <hello@travislima.com>
2018-11-08 14:56:49 +0200

    Separated CSS and JS from Index.html
    Separated CSS and JS from Index.html and made two separate files for each.

---
+++ Pomodo/scripts.js
@@ -0,0 +1,45 @@
+
+
+window.onload = function() {
+    //variables
+    var form = document.getElementById("pomodo-form");
+    var input = document.getElementById("pomodo-input");
+    var btn = document.getElementById("pomodo-btn");
+    var list = document.getElementById("pomodo-list");
+    var id = 1;
+
+    //button event listener
+    btn.addEventListener("click", addTodoItem);
+
+    //list event listener
+    list.addEventListener("click", boxChecked);
+
+    //add todo item to list
+    function addTodoItem() {
+        if(input.value === "") {
+            alert("Please enter a To Do List Item.");
+        }
+        else {
+            if(list.style.borderTop === "") {
+                list.style.borderTop = "2px solid #fff";
+            }
+            var text = input.value;
+            var item = `<li id="li-${id}">${text}
+                        <input id="box-${id}" 
+                               class="checkboxes"
+                               type="checkbox"></li>`;
+            list.insertAdjacentHTML('beforeend', item);
+            id++;
+            input.value = ""; // reset the input value when button is clicked + not empty.
+        }
+    }
+
+    //Add Strikethrough on completed to do list items.
+    function boxChecked(event) {
+    	const element = event.target;
+    	if(element.type === "checkbox") {
+    		element.parentNode.style.textDecoration = "line-through";
+    	}
+    }
+}
+
