18e2c35fafdc138aad43b9eba227db3d291f3b5f
Travis Lima <hello@travislima.com>
2020-05-11 22:24:49 +0200

    Update scripts.js
    Making a countdown timer from 25minutes down to zero.

--- Pomodo/scripts.js
+++ Pomodo/scripts.js
@@ -18,7 +18,7 @@ window.onload = function() {
 
     //list event listener
     list.addEventListener("click", boxChecked);
-
+    
     //add todo item to list
     function addTodoItem() {
         if(input.value === "") {
@@ -51,4 +51,23 @@ window.onload = function() {
             
         }
     }
+}
+
+// Countdown timer code
+
+const startingMinutes = 25;
+let time = startingMinutes * 60;
+
+const countdownEL = document.getElementById("countdownTimer");
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEL.innerHTML = `${minutes}: ${seconds}`;
+    time--;
 }
